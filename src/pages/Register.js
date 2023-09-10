96c9b777605f45e12c90c9cdc9297f73dfaa49c5
atikP <atikpatel@hotmail.co.uk>
2023-09-10 15:20:54 +0100

    ENV refresh

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
 import { register } from "../services";
 import { useTitle } from "../hooks/useTitle";
+import { toast } from "react-toastify";
 
 
 export const Register = () => {
